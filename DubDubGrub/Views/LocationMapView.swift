9c3fdafd2f119e283bf3f2a2449d5fe93d682301
Simon Berner <sibebzh@gmail.com>
2021-12-20 21:03:15 +0100

    feat(mapview): :sparkles: setup the MapView with MapKit

--- DubDubGrub/Views/LocationMapView.swift
+++ DubDubGrub/Views/LocationMapView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+
+    // set Apple WWDC Convention Center as location
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                          longitudeDelta: 0.01))
     var body: some View {
-        Text("LocationMap")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea(edges: .top)
+
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                .frame(height: 70)
+                .shadow(radius: 10)
+
+                Spacer()
+            }
+        }
     }
 }
 
