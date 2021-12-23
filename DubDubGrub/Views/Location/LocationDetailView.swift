6a471a6615f0159860efe6fa4fa73afbb86d339a
Simon Berner <sibebzh@gmail.com>
2021-12-23 22:58:28 +0100

    refactor(view): :hammer: refactor the LocationDetailView
    - Put the LazyVGrid into a ScrollView
    - Put the LocationDetailView Preview into an NavigationView

--- DubDubGrub/Views/Location/LocationDetailView.swift
+++ DubDubGrub/Views/Location/LocationDetailView.swift
@@ -13,60 +13,61 @@ struct LocationDetailView: View {
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
-                HStack {
-                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            HStack {
+                Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
 
-                    Spacer()
-                }
+            Text("This is a test description. This is a test description. This is a test description. This is a test description. ")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
 
-                Text("This is a test description. This is a test description. This is a test description. This is a test description. ")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
+            GroupBox {
+                HStack(spacing: 20) {
+                    Button {
 
-                GroupBox {
-                    HStack(spacing: 20) {
-                        Button {
-
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
-
-                        Link(destination: URL(string: "https://www.apple.com")!, label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
-                        })
-
-                        Button {
-
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
-
-                        Button {
-
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.xmark")
-                        }
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
                     }
 
+                    Link(destination: URL(string: "https://www.apple.com")!, label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
+                    })
+
+                    Button {
+
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
+
+                    Button {
+
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "person.fill.xmark")
+                    }
                 }
-                .foregroundColor(Color(uiColor: .secondarySystemBackground))
-                .clipShape(RoundedRectangle(cornerRadius: 60, style: .continuous))
-                .padding(.horizontal)
 
-                Text("Who's here?")
-                    .bold()
-                    .font(.title2)
+            }
+            .foregroundColor(Color(uiColor: .secondarySystemBackground))
+            .clipShape(RoundedRectangle(cornerRadius: 60, style: .continuous))
+            .padding(.horizontal)
 
+            Text("Who's here?")
+                .bold()
+                .font(.title2)
+
+            ScrollView {
                 // only 10 views can be placed in the grid
                 LazyVGrid(columns: columns, content: {
                     FirstNameAvatarView(firstName: "Simon")
@@ -79,9 +80,8 @@ struct LocationDetailView: View {
                     FirstNameAvatarView(firstName: "Simon")
                     FirstNameAvatarView(firstName: "Simon")
                 })
-
-                Spacer()
             }
+            Spacer()
         }
         .navigationTitle("Location Name")
         .navigationBarTitleDisplayMode(.inline)
@@ -127,7 +127,10 @@ struct FirstNameAvatarView: View {
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        // Tip: Put the Preview into a NavigationView to see how it looks like
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 
