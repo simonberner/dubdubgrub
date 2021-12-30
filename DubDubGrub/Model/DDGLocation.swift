aafb2e2fefd12b7839d45771960919c51e435645
Simon Berner <sibebzh@gmail.com>
2021-12-30 22:20:22 +0100

    add(model): :sparkles: add the DDGLocation model

---
+++ DubDubGrub/Model/DDGLocation.swift
@@ -0,0 +1,46 @@
+//
+//  DDGLocation.swift
+//  DubDubGrub
+//
+//  Created by Simon Berner on 30.12.21.
+//
+
+import CloudKit
+
+// Object which maps to the DDGLocation CloudKit Record Type
+struct DDGLocation {
+
+    static let kName = "Name"
+    static let kDescription = "description"
+    static let kSquareAsset = "squareAsset"
+    static let kBannerAsset = "bannerAsset"
+    static let kAddress = "address"
+    static let kLocation = "location"
+    static let kWebsiteURL = "websiteURL"
+    static let kPhoneNumber = "phoneNumber"
+
+    let ckRecordId: CKRecord.ID
+    let name: String
+    let description: String
+    let squareAsset: CKAsset! //implicitly unwrapping of the optional (handling the nil case later on with a default image asset)
+    let bannerAsset: CKAsset!
+    let address: String
+    let location: CLLocation
+    let websiteURL: String
+    let phoneNumber: String
+
+    // creating our own initializer because we get a CKRecord back when querying CloudKit database
+    init(record: CKRecord) {
+        ckRecordId = record.recordID
+        name = record[DDGLocation.kName] as? String ?? "N/A" // record is a key-value pair
+        description = record[DDGLocation.kDescription] as? String ?? "N/A"
+        squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
+        bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
+        address = record[DDGLocation.kAddress] as? String ?? "N/A"
+        location = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        websiteURL = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
+        phoneNumber = record[DDGLocation.kPhoneNumber] as? String ?? "N/A"
+
+    }
+
+}
