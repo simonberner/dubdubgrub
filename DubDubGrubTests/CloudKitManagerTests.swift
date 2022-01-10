0ddfd236b3cc5d24d2e2f9f9b41f1ffb6fb7f124
Simon Berner <sibebzh@gmail.com>
2022-01-10 08:49:32 +0100

    test(ck)::white_check_mark: setup the CloudKitManagerTests

--- DubDubGrubTests/CloudKitManagerTests.swift
+++ DubDubGrubTests/CloudKitManagerTests.swift
@@ -8,14 +8,23 @@
 import XCTest
 @testable import DubDubGrub
 
-class DubDubGrubTests: XCTestCase {
+class CloudKitManagerTests: XCTestCase {
 
+    // This method is called before the invocation of each test method in the class.
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+//        try.super.setUpWithError()
     }
 
+    // This method is called after the invocation of each test method in the class.
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+    }
+
+    func testLocationsFetching() async throws {
+        // Arrange & Act
+//        let locations = try await getLocation()
+        // Assert
+//        XCTAssertNotNil(locations)
     }
 
     func testExample() throws {
