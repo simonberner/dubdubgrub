e057d67f5ae0a6e01bc42290e3ee1445a324deac
Simon Berner <sibebzh@gmail.com>
2022-01-31 21:05:43 +0100

    add(mapannotation): :sparkles: add a DDGAnnotation view

---
+++ DubDubGrub/Views/CustomViews/MapAnnotation/DDGAnnotation.swift
@@ -0,0 +1,47 @@
+//
+//  DDGAnnotation.swift
+//  DubDubGrub
+//
+//  Created by Simon Berner on 31.01.22.
+//
+
+import SwiftUI
+
+struct DDGAnnotation: View {
+
+    var location: DDGLocation
+
+    var body: some View {
+        VStack {
+            ZStack {
+                MapBallon()
+                    .frame(width: 100, height: 70)
+                    .foregroundColor(.brandPrimary)
+
+                Image(uiImage: location.getImage(for: .square))
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                    .offset(y: -10)
+
+                Text("99")
+                    .font(.system(size: 11, weight: .bold))
+                    .frame(width: 26, height: 18)
+                    .background(Color.grubRed)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .offset(x: 20, y: -28)
+            }
+
+            Text(location.name)
+                .font(.caption)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+struct DDGAnnotation_Previews: PreviewProvider {
+    static var previews: some View {
+        DDGAnnotation(location: DDGLocation(record: MockData.location))
+    }
+}
